c67c258199315fd58bbadc10466ad8262e68c1c2
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2022-10-03 22:32:21 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,41 @@
-import React from "react";
-import { useState } from "react";
-import AdditivesMass from "./comp/Menu/Citchen/Аdditives/Additives";
-import { Additives } from "./comp/Menu/Citchen/Аdditives/Аdditives";
-// import s from './App.module.css'
+import React from 'react';
+import { useState } from 'react';
+import { Burgers } from './comp/Menu/Citchen/Burgers/Burgers';
+import BurgerMass from './comp/Menu/Citchen/Burgers/DataBurg';
 
-import { Modal } from "./Modal/Modal";
+import AdditivesMass from './comp/Menu/Citchen/Аdditives/DataAdd';
+import { Additives } from './comp/Menu/Citchen/Аdditives/Аdditives';
+
+import s from './App.module.css'
+
+import { Modal } from './Modal/Modal';
 
 export const App = () => {
-
   const [showModal, setShowModal] = useState(false);
   const [objectModal, setObjectModal] = useState({});
 
   const dataModal = (title, price, text, src) => {
-  
     toggleModal();
     setObjectModal({ title, price, text, src });
   };
   const toggleModal = () => {
- 
     setShowModal(showModal => !showModal);
   };
 
-  const dataAdd = AdditivesMass
- 
-    return (
-      <>
-   
-      
+  const dataAdd = AdditivesMass;
+  const dataBurg = BurgerMass;
+
+  return (
+    <>
+    <div className={s.section}>
       <Additives data={dataAdd} onModal={dataModal}/>
-      {showModal && <Modal objectModal={objectModal} toggleModal={toggleModal}/>}
-      </>
-    )
-  }
+      <Burgers data={dataBurg} onModal={dataModal} className={s.item} />
+    </div>
+      
+
+      {showModal && (
+        <Modal objectModal={objectModal} toggleModal={toggleModal} />
+      )}
+    </>
+  );
+};
--- src/components/comp/Menu/Citchen/Аdditives/Аdditives.jsx
+++ src/components/comp/Menu/Citchen/Аdditives/Аdditives.jsx
@@ -1,17 +1,8 @@
-
-// import AdditivesMass from "./Additives";
-
-
 export const Additives = ({data, onModal}) => {
 
-    
-  
     return data.map(e=>(
-      <ul>
-        <li key={e.id} onClick={()=>onModal(e.title, e.price, e.text, e.src)}>{e.title}</li>
-        {/* <li>
-          <img src={require('./images/123123.png')} alt="" />
-        </li> */}
+      <ul key={e.id}>
+        <li  onClick={()=>onModal(e.title, e.price, e.text, e.src)}>{e.title}</li>
       </ul>
       
     ));
