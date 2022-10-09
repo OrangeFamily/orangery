de6fb765e0eca6b5bb3a5a9b550e8620d21d5653
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2022-10-09 11:36:59 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,29 +6,30 @@ import {
 } from '@chakra-ui/react';
 
 import { useState } from 'react';
-// import { Burgers } from './comp/Menu/Citchen/Burgers/Burgers';
-// import BurgerMass from './comp/Menu/Citchen/Burgers/DataBurg';
 
-// import AdditivesMass from './comp/Menu/Citchen/Аdditives/DataAdd';
-// import { Additives } from './comp/Menu/Citchen/Аdditives/Аdditives';
 import { Modal } from './Modal/Modal';
 import s from './App.module.scss';
 import { MeatAss } from './comp/Menu/Citchen/MeatAss/MeatAss';
 import MeatAssMass from './comp/Menu/Citchen/MeatAss/DataMeatAss';
 import { Salads } from './comp/Menu/Citchen/Salads/Salads';
 import SaladsMass from './comp/Menu/Citchen/Salads/DataSalads';
-import FrutMass from './comp/Menu/Citchen/Cold/DataFrut';
-import { Frut } from './comp/Menu/Citchen/Cold/Frut';
+import FrutMass from './comp/Menu/Citchen/Frut/DataFrut';
+import { Frut } from './comp/Menu/Citchen/Frut/Frut';
+import { Burgers } from './comp/Menu/Citchen/Burgers/Burgers';
+import BurgerMass from './comp/Menu/Citchen/Burgers/DataBurg';
+import HotMass from './comp/Menu/Citchen/Hot/DataHot';
+import { Hot } from './comp/Menu/Citchen/Hot/Hot';
 
 export const App = () => {
   const [showModal, setShowModal] = useState(false);
   const [objectModal, setObjectModal] = useState({});
 
-  // const dataAdd = AdditivesMass;
+  
   const dataFrut = FrutMass;
-  // const dataBurg = BurgerMass;
+  const dataBurgers = BurgerMass;
   const dataMeatAss = MeatAssMass;
   const dataSalads = SaladsMass;
+  const dataHot = HotMass;
 
   const dataModal = (title, price, text, src) => {
     toggleModal();
@@ -87,6 +88,26 @@ export const App = () => {
                   </h2>
                   <AccordionPanel>
                     <Salads data={dataSalads} onModal={dataModal} />
+                  </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton className={s.titleItem}>
+                      Бургери
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel>
+                    <Burgers data={dataBurgers} onModal={dataModal} />
+                  </AccordionPanel>
+                </AccordionItem>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton className={s.titleItem}>
+                      Гарячі закуски
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel>
+                    <Hot data={dataHot} onModal={dataModal} />
                   </AccordionPanel>
                 </AccordionItem>
               </Accordion>
--- src/components/comp/Menu/Citchen/Burgers/Burgers.jsx
+++ src/components/comp/Menu/Citchen/Burgers/Burgers.jsx
@@ -1,9 +1,23 @@
-// import s from './Burgers.module.css'
+import s from './Burgers.module.scss'
  export const Burgers = ({data, onModal}) => {
 
     return data.map(e=>(
      
-        <li key={e.id} onClick={()=>onModal(e.title, e.price, e.text, e.src)}>{e.title}</li>
+        <div
+      key={e.id}
+      onClick={() => onModal(e.title, e.price, e.text, e.src)}
+      className={s.item}
+    ><img src={e.src} alt="" className={s.sizeImg} />
+      <div>
+        <div className={s.itemMain}><h3 className={s.title}>{e.title}</h3>
+
+        <h3 className={s.price}>{e.price}</h3></div>
+        
+        <p className={s.text}>{e.text}</p>
+      </div>
+
+      
+    </div>
     
       
     ));
