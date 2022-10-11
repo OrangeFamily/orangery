713470fe7fcb1b389a6fa310f4992e612de3c275
Олексій Демидов <96746949+AlekseyDemydov@users.noreply.github.com>
2022-10-11 23:11:59 +0300

    fix

--- src/components/comp/Kitchen/Data/DataFirst.js
+++ src/components/comp/Kitchen/Data/DataFirst.js
@@ -3,7 +3,21 @@ const FirstMass=[
         id:1,
         title:"Бульйон з куркою 250г",
         price:"49 грн",
-        src: require('../img/123123.png'),
+        src: require('../img/Sup/chiken.png'),
+        text:"Бульйон, філе куряче, лакшина",
+    },
+    {
+        id:2,
+        title:"грибний крем-суп 250г",
+        price:"49 грн",
+        src: require('../img/Sup/mush.png'),
+        text:"Бульйон, філе куряче, лакшина",
+    },
+    {
+        id:3,
+        title:"солянка м'ясна 250г",
+        price:"49 грн",
+        src: require('../img/Sup/solyanka.png'),
         text:"Бульйон, філе куряче, лакшина",
     },
     
