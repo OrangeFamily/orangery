22305995b24449d4c3f0588d18fbc2dc2098f29e
Олексій Демидов <orange.family.cn@gmail.com>
2023-03-29 22:01:12 +0300

    fix

--- src/components/comp/Bar/Data/DataEnergy.js
+++ src/components/comp/Bar/Data/DataEnergy.js
@@ -2,7 +2,7 @@ const EnergyMass=[
     {
         id:1,
         title:"Red Bull 0,25л",
-        price:"80 грн",
+        price:"90 грн",
         src: require('../img/redbull.png'),
         text:"",
     },
