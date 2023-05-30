d5668ec7724005c5da28124052b3dbb217da9123
Олексій Демидов <orange.family.cn@gmail.com>
2023-05-31 00:39:31 +0300

    Update DataCoffeCold.js

--- src/components/comp/Bar/Data/DataCoffeCold.js
+++ src/components/comp/Bar/Data/DataCoffeCold.js
@@ -1,5 +1,11 @@
 const CoffeColdMass=[
-    
+    {
+        id:3,
+        title:"Бамбл кава 0,250л",
+        price:"69 грн",
+        src: require('../img/cof.png'),
+        text:"",
+    },
     {
         id:1,
         title:"Глясе 0,250л",
@@ -14,8 +20,6 @@ const CoffeColdMass=[
         src: require('../img/cof.png'),
         text:"",
     },
-    
-    
     
 ]
 
