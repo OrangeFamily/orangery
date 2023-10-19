e3af92ece8aa4bef3974ffaa9e1a24b8b6f40089
OrangeFamily <111183461+OrangeFamily@users.noreply.github.com>
2023-10-19 16:40:32 +0300

    Update DataEger.js

--- src/components/comp/Bar/Data/DataEger.js
+++ src/components/comp/Bar/Data/DataEger.js
@@ -2,9 +2,9 @@ const EgerMass=[
     {
         id:1,
         title:"Егермайстер (Німеччина) 1л",
-        price:"1400 грн",
+        price:"1760 грн",
         src: require('../img/jager.png'),
-        text:"50мл - 70 грн",
+        text:"50мл - 88 грн",
     },
     
 ]
